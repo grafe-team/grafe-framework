78a83386e49cd2a63eab1cb3df728b611f782321
MawinWeatha <g3Nd757SVv83>
2021-05-07 23:37:09 +0200

    :construction: generate() :  basic flow

--- grafe-cli/src/generate/generate.ts
+++ grafe-cli/src/generate/generate.ts
@@ -1,30 +1,73 @@
-import inquierer from 'inquirer';
+import inquirer from 'inquirer';
+import { choices } from 'yargs';
+import fs from 'fs';
 
 function generate() {
-    inquierer.prompt([
+    inquirer.prompt([
         {
-            type: 'expand',
+            type: 'list',
             name: 'type',
-            message: 'What do you want to do?',
-            choices: [
+            message: 'What do you want to generate',
+            choices: ['Route', 'Middleware'],
+        }
+    ]).then(answers => {
+        if (answers.type === 'Route') {
+            inquirer.prompt([
                 {
-                    key: 'p',
-                    name: 'Pepperoni and cheese',
-                    value: 'PepperoniCheese',
+                    type: 'input',
+                    name: 'path',
+                    message: 'How is the new route called'
                 },
                 {
-                    key: 'a',
-                    name: 'All dressed',
-                    value: 'alldressed',
+                    type: 'list',
+                    name: 'method',
+                    message: 'Which HTTP-Method should the new route be',
+                    choices: ['GET', 'POST', 'PUT', 'DELETE'],
                 },
                 {
-                    key: 'w',
-                    name: 'Hawaiian',
-                    value: 'hawaiian',
-                },
-            ],
-        },
-    ]);
+                    type: 'checkbox',
+                    message: 'Select the middlewares for this route',
+                    name: 'middlewares',
+                    choices: [
+                        {
+                            name: 'not protected',
+                            value: 'np'
+                        }, 
+                        {
+                            name: 'only as user',
+                            value: 'user'
+                        }
+                    ]
+                }
+            ]).then(answers => {
+                console.log(answers);
+                let paths = answers.path.split('/');
+                console.log(paths);
+                
+                let path = "./src/";
+                for(let pathSplit of paths) {
+                    if(!(pathSplit === paths[paths.length - 1])) {
+                        path += pathSplit + "/";
+                    }
+                }
+
+                let middlewares = answers.middlewares;
+
+                if(middlewares.length == 1) {
+                    path += middlewares[0] + "/";
+                }else {
+                    for(let i = 0; i < middlewares.length - 1; i++) {
+                        path += middlewares[i] + ".";
+                    }
+                    path += middlewares[middlewares.legth - 1];
+                }
+
+                path += paths[paths.length - 1] + "." + answers.method.toLowerCase() + ".ts";
+
+                console.log(path);
+            });
+        }
+    });
 }
 
 export = generate;
--- grafe-cli/src/index.ts
+++ grafe-cli/src/index.ts
@@ -1,3 +1,8 @@
 #! /usr/bin/env node
+
+import generate from './generate/generate';
+
 console.log("Hello World!");
 console.log("Hello World!");
+
+generate();

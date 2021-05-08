921c2564cbe1b715ccf3ba111910e5522122f4a4
streimelstefan <stefan@streimel.com>
2021-05-08 16:07:36 +0200

    :construction: created basic templating code that works for starter templates

--- grafe-cli/src/start/start.ts
+++ grafe-cli/src/start/start.ts
@@ -1,20 +1,56 @@
 import yargs from 'yargs';
-import inquirer from 'inquirer'
+import inquirer from 'inquirer';
+import * as fs from 'fs';
+import * as path from 'path';
+import { StarterTemplateOptions, createDirectoryContents } from '../utils/templating'
 
 export function startCommand(yargs: yargs.Argv<{}>) {
     return yargs;
 }
 
-export function startHandler(argv: any) {
-    inquirer.prompt([
+export async function startHandler(argv: any) {
+
+    const templateStartersPath = path.join(__dirname, '..', '..', 'templates', 'starters'); 
+
+    const templateChoises = fs.readdirSync(templateStartersPath);
+
+    const answers = await inquirer.prompt([
         {
             type: 'input',
-            message: 'Whats the name of your project',
+            message: 'Whats the name of your project?',
             name: 'projectName'
+        },
+        {
+            type: 'list',
+            name: 'templateType',
+            message: 'What project template would you like to use?',
+            choices: templateChoises
         }
-    ]).then((value: any) =>{
-        console.log(value);
-    }).catch((error: any) => {
-        console.error(error);
-    })
+    ]);
+
+    const projectOptions: StarterTemplateOptions = {
+        projectName: answers.projectName,
+        projectPath: path.join(process.cwd(), answers.projectName),
+        templateName: answers.templateType,
+        templatePath: path.join(templateStartersPath, answers.templateType)
+    };
+    console.log(projectOptions);
+
+    if (!createProjectFolder(projectOptions)) {
+        return;
+    }
+
+    createDirectoryContents(projectOptions.templatePath, projectOptions.projectName, projectOptions);
+
+}
+
+function createProjectFolder(options: StarterTemplateOptions): boolean {
+    if (fs.existsSync(options.projectPath)) {
+        console.error(`Folder ${options.projectPath} already exists. Delete it or use another name!`);
+        return false;
+    }
+
+    fs.mkdirSync(options.projectPath);
+
+    return true;
 }
---
+++ grafe-cli/src/utils/templating.ts
@@ -0,0 +1,45 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as ejs from 'ejs';
+
+
+export interface StarterTemplateOptions {
+    templatePath: string;
+    templateName: string;
+    projectPath: string;
+    projectName: string;
+}
+
+export function createDirectoryContents(templatePath: string, currentFolder: string, options: any) {
+    const SKIP_FILES = ['node_modules', '.template.json'];
+
+    // read all files/folders (1 level) from template folder
+    const filesToCreate = fs.readdirSync(templatePath);
+    // loop each file/folder
+    filesToCreate.forEach(file => {
+        const origFilePath = path.join(templatePath, file);
+        
+        // get stats about the current file
+        const stats = fs.statSync(origFilePath);
+    
+        // skip files that should not be copied
+        if (SKIP_FILES.indexOf(file) > -1) return;
+        
+        if (stats.isFile()) {
+            // read file content and transform it using template engine
+            let contents = fs.readFileSync(origFilePath, 'utf8');
+
+            // insert dynamic content 
+            contents = ejs.render(contents, options);
+
+            // write file to destination folder
+            const writePath = path.join(process.cwd(), currentFolder, file);
+            fs.writeFileSync(writePath, contents, 'utf8');
+        } else if (stats.isDirectory()) {
+            // create folder in destination folder
+            fs.mkdirSync(path.join(process.cwd(), currentFolder, file));
+            // copy files/folder inside current folder recursively
+            createDirectoryContents(path.join(templatePath, file), path.join(currentFolder, file), options);
+        }
+    });
+}

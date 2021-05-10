2851db1786e122a9a5d2e20ca29bd24b5f36069b
streimelstefan <stefan@streimel.com>
2021-05-10 12:07:57 +0200

    :construction: Added removeNonExistantMiddlewares wich removes mw from the config where the file that the link points to does not exist

--- grafe-core/src/initCore.ts
+++ grafe-core/src/initCore.ts
@@ -23,7 +23,7 @@ export function initCore(configPath: string): boolean {
 
     let config: Config;
 
-    // read the config file and set the global config
+    // read the config file and parse it
     try {
         config = JSON.parse(fs.readFileSync(configPath).toString());
     } catch(error) {
@@ -34,18 +34,61 @@ export function initCore(configPath: string): boolean {
     // set base dir in the config
     config.baseDir = path.parse(configPath).dir;
 
+    // create links to the mws
     config = createMiddlewareLinks(config);
+
+    // remove middlewares that do not exist
+    config = removeNonExistantMiddlewares(config);
 
     // set the created config so it can be used globaly
     setConfig(config);
 
+
     return true;
 }
 
+/**
+ * Creates the links to the file of a middleware. The link is the path to the file where the middleware is saved
+ * 
+ * @param config the grafe config
+ * @returns the new grafe config with the middleware links
+ */
 function createMiddlewareLinks(config: Config): Config {
     config.middlewares.forEach(mw => {
+        // build the link 
+        // middlewares are stored in the path: 'BASE_DIR/MW_STORAGE_PATH/MW_SHORT/MW_NAME.js
+        // exapmle: BASEDIR/src/middlewares/usr/users_only.js
         mw.link = path.join(config.baseDir, config.middlewarePath, mw.value, mw.name + '.js');
     });
+
+    return config;
+}
+
+/**
+ * Checks if the middleware files exist. If they dont exist they will be removed from the list.
+ * 
+ * !The middleware links need to be constructed or errors will be thrown
+ * 
+ * @param config The grafe config
+ * @returns The new grafe config
+ */
+function removeNonExistantMiddlewares(config: Config): Config {
+    for (let i = 0; i < config.middlewares.length; i++) {
+        
+        // if a link is undefind throw an error
+        if (config.middlewares[i].link === undefined) {
+            throw `No link for middleware ${config.middlewares[i].name} found. Where the links build?`;
+        }
+
+        // check if the middleware file exists
+        if (!fs.existsSync(config.middlewares[i].link)) {
+            // remove the mw because it does not exist
+            config.middlewares.splice(i, 1);
+
+            // decrease i by one so we dont skip a mw
+            i--;
+        }
+    }
 
     return config;
 }
--- grafe-core/src/test/initCore.test.ts
+++ grafe-core/src/test/initCore.test.ts
@@ -54,7 +54,7 @@ describe('initCore file', () => {
             const resulst = initCore(path.join(basePath, 'grafe.json'));
 
             // check if all functions that should/shouldnt be called where called acordingly
-            chai.expect(fsExistsStub.callCount).to.deep.eq(1, 'fs.existsSync should be called once');
+            chai.expect(fsExistsStub.callCount).to.deep.eq(2, 'fs.existsSync should be called once');
             chai.expect(fsReadFileStub.callCount).to.deep.eq(1, 'fs.readFileSync should be called once');
             chai.expect(setConfigSpy.callCount).to.deep.eq(1, 'setConfig should be called once');
             chai.expect(consoleErrorStub.called).to.deep.eq(false, 'console.error should not be called');
@@ -172,6 +172,123 @@ describe('initCore file', () => {
 
             chai.expect(result).to.be.deep.equal(config);
 
+        });
+
+    });
+
+    describe('removeNonExistantMiddlewares function', () => {
+
+        let fsExistsStub: sinon.SinonStub;
+
+        const configStore: Config.Config = {
+            baseDir: path.join(__dirname),
+            middlewarePath: 'src/test/middleware',
+            projectType: 'express',
+            routePath: 'something',
+            middlewares: [
+                {
+                    description: 'item 1',
+                    name: 'i1',
+                    value: 'i1',
+                    link: ''
+                },
+                {
+                    description: 'item 2',
+                    name: 'i2',
+                    value: 'i2',
+                    link: ''
+                },
+                {
+                    description: 'item 3',
+                    name: 'i3',
+                    value: 'i3',
+                    link: ''
+                }
+            ]
+        };
+
+        let config: Config.Config;
+
+        beforeEach(() => {
+            fsExistsStub = sinon.stub(fs, 'existsSync');
+            // deepl clone in a costly way :)
+            config = JSON.parse(JSON.stringify(configStore));
+        });
+
+        afterEach(() => {
+            fsExistsStub.restore();
+        });
+
+        it ('should remove the second an third mw without throwing an error', () => {
+            const initCoreModule = rewire('../initCore');
+
+            fsExistsStub.onCall(0).returns(true);
+            fsExistsStub.onCall(1).returns(false);
+            fsExistsStub.onCall(2).returns(false);
+
+            const removeNonExistantMiddlewares = initCoreModule.__get__('removeNonExistantMiddlewares');
+            
+            
+            let resultConfig: Config.Config;
+
+            // check if the function throws an error
+            chai.expect(() => {resultConfig = removeNonExistantMiddlewares(config)}).to.not.throw();
+
+            // check if the returned config is as expected 
+            chai.expect(fsExistsStub.callCount).to.deep.eq(3, 'fs.existsSync should be called 3 times');
+            chai.expect(resultConfig.middlewares.length).to.deep.eq(1, 'there should only be 1 mw left');
+            chai.expect(resultConfig.middlewares[0]).to.deep.eq(configStore.middlewares[0], 'The last middleware standing should be the same as the first one');
+        });
+
+        it ('should remove the first and second mw without throwing an error', () => {
+            const initCoreModule = rewire('../initCore');
+
+            fsExistsStub.onCall(0).returns(false);
+            fsExistsStub.onCall(1).returns(false);
+            fsExistsStub.onCall(2).returns(true);
+
+            const removeNonExistantMiddlewares = initCoreModule.__get__('removeNonExistantMiddlewares');
+            
+            let resultConfig: Config.Config;
+
+            // check if the function throws an error
+            chai.expect(() => {resultConfig = removeNonExistantMiddlewares(config)}).to.not.throw();
+
+            // check if the returned config is as expected 
+            chai.expect(fsExistsStub.callCount).to.deep.eq(3, 'fs.existsSync should be called 3 times');
+            chai.expect(resultConfig.middlewares.length).to.deep.eq(1, 'there should only be 1 mw left');
+            chai.expect(resultConfig.middlewares[0]).to.deep.eq(configStore.middlewares[2], 'The last middleware standing should be the same as the thrid one');
+        });
+
+        it ('should remove all mws without throwing an error', () => {
+            const initCoreModule = rewire('../initCore');
+
+            fsExistsStub.returns(false);
+
+            const removeNonExistantMiddlewares = initCoreModule.__get__('removeNonExistantMiddlewares');
+            
+            let resultConfig: Config.Config;
+
+            // check if the function throws an error
+            chai.expect(() => {resultConfig = removeNonExistantMiddlewares(config)}).to.not.throw();
+
+            // check if the returned config is as expected 
+            chai.expect(fsExistsStub.callCount).to.deep.eq(3, 'fs.existsSync should be called 3 times');
+            chai.expect(resultConfig.middlewares.length).to.deep.eq(0, 'there should no mw left');
+        });
+
+        it ('should throw an error if a link is undefined', () => {
+            const initCoreModule = rewire('../initCore');
+
+            // set the links to undefined
+            config.middlewares.forEach(mw => {
+                mw.link = undefined;
+            });
+
+            const removeNonExistantMiddlewares = initCoreModule.__get__('removeNonExistantMiddlewares');
+
+            // check if the function throws an error
+            chai.expect(() => {removeNonExistantMiddlewares(config)}).to.throw();
         });
 
     });

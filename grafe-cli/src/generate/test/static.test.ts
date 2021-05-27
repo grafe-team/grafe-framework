edec42a4aa780cf44b8fd16e9594985f3e381ef9
kochmarvin <marvin.info@gmx.at>
2021-05-27 15:14:19 +0200

    :white_check_mark: Added tests for generateStaticHandler()

--- grafe-cli/src/generate/test/static.test.ts
+++ grafe-cli/src/generate/test/static.test.ts
@@ -125,4 +125,89 @@ describe('static.generate.ts file', () => {
             chai.expect(mkdirpStub.lastCall.args[0]).to.deep.eq('C:\\grafe\\project_1\\src\\static\\test', 'should be this exact path');
         });
     });
+
+    describe('generateStaticHandler function', () => { 
+        
+        const consoleErrorStub = Sinon.stub();
+        const existsSyncStub = Sinon.stub();
+        const promptStub = Sinon.stub();
+        const mkdirpStub = Sinon.stub();
+        const pkgDirStub = Sinon.stub();
+        const generateStaticStub = Sinon.stub();
+
+        let generateStaticHandler: (argv: any) => Promise<void>
+                = staticGenerate.__get__('generateStaticHandler');
+
+        const fsMock = {
+            existsSync: existsSyncStub,
+        };
+
+        const inquirerMock = {
+            prompt: promptStub
+        };
+
+        const pkgDirMock = {
+            default: pkgDirStub
+        }
+
+        beforeEach(() => {
+            consoleErrorStub.reset();
+
+            staticGenerate.__set__({
+                generateStatic: generateStaticStub,
+            });
+
+            staticGenerate.__set__({
+                console: {
+                    error: consoleErrorStub
+                }
+            });
+
+            staticGenerate.__set__({
+                fs: fsMock,
+                inquirer: inquirerMock,
+                pkgDir: pkgDirMock
+            });
+
+            generateStaticHandler = staticGenerate.__get__('generateStaticHandler');
+
+            promptStub.reset();
+            mkdirpStub.reset();
+            pkgDirStub.reset();
+            existsSyncStub.reset();
+        });
+
+        it('should log an error when not in a grafe project', async () => {
+            pkgDirStub.resolves('C:\\grafe\\project_1');
+            existsSyncStub.returns(false);
+
+            await generateStaticHandler({});
+
+            chai.expect(consoleErrorStub.callCount).to.deep.eq(1, 'console.error should be called once');
+            chai.expect(consoleErrorStub.calledOnceWith(messages.not_grafe)).to.be.eq(true, 'console.error should be called with not_grafe message');
+        });
+
+        it('should prompt the user when name not given', async () => {
+            pkgDirStub.resolves('C:\\grafe\\project_1');
+            existsSyncStub.returns(true);
+            promptStub.resolves({name: 'Test'});
+
+            await generateStaticHandler({});
+
+            chai.expect(consoleErrorStub.callCount).to.deep.eq(0, 'console.error should not be called once');
+            chai.expect(promptStub.callCount).to.deep.eq(1, 'user should be prompted once');
+            chai.expect(generateStaticStub.lastCall.args[0]).to.deep.eq('Test', 'should be the string of the prompt');
+        });
+
+        it('should not prompt the user when name given', async () => {
+            pkgDirStub.resolves('C:\\grafe\\project_1');
+            existsSyncStub.returns(true);
+
+            await generateStaticHandler({ name: 'Test' });
+
+            chai.expect(consoleErrorStub.callCount).to.deep.eq(0, 'console.error should not be called once');
+            chai.expect(promptStub.callCount).to.deep.eq(0, 'user should not be prompted');
+            chai.expect(generateStaticStub.lastCall.args[0]).to.deep.eq('Test', 'should be the string of the prompt');
+        });
+    });
 });

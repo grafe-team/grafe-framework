6dd08829de2692623423c1219ff424e0f8f9e054
kochmarvin <marvin.info@gmx.at>
2021-05-27 13:20:44 +0200

    :hammer: generateStatic(): added more checks

--- grafe-cli/src/generate/static.generate.ts
+++ grafe-cli/src/generate/static.generate.ts
@@ -1,7 +1,7 @@
-import inquirer from 'inquirer';
-import fs from 'fs';
-import mkdirp from 'mkdirp';
-import pkgDir from 'pkg-dir';
+import * as inquirer from 'inquirer';
+import * as fs from 'fs';
+import * as mkdirp from 'mkdirp';
+import * as pkgDir from 'pkg-dir';
 import * as path from 'path';
 import messages from './generate.messages';
 
@@ -12,18 +12,18 @@ import messages from './generate.messages';
  * @returns Promise<undefined>
  */
  export async function generateStaticHandler(argv: any): Promise<void> {
+    
     // get root directory (where package.json is in)
-    const rootDir = await pkgDir(process.cwd());
-
-    // check if the project is a grafe project
-    try {
-        fs.readFileSync(path.join(rootDir, '/grafe.json'));
-    } catch (err) {
-        return console.log(messages.not_grafe);
+    const rootDir = await pkgDir.default(process.cwd());
+    
+    // check if in grafe project
+    if(!fs.existsSync(path.join(rootDir, 'grafe.json'))) {
+        return console.error(messages.not_grafe);
     }
 
     let questions = [];
 
+    // if name is not given then
     if (argv.name == undefined) {
         questions.push({
             type: 'input',
@@ -52,23 +52,40 @@ import messages from './generate.messages';
  */
 export async function generateStatic(name: string): Promise<void> {
 
+    // prompt confirmation to user
     let confirm = await inquirer.prompt({
         message: messages.confirm,
         type: 'confirm',
         name: 'confirm'
     });
 
+    // if not confirming abort
     if(!confirm.confirm) {
         return;
     }
 
+    // check if length is greater then 0
+    if(name.length == 0) {
+        return console.error(messages.generateStatic.to_small);
+    }
+
+    // check if name has a ':' in it
+    if(name.indexOf(':') != -1) {
+        return console.error(messages.generateStatic.no_colon);
+    }
+
     // get root directory (where package.json is in)
-    const rootDir = await pkgDir(process.cwd());
+    const rootDir = await pkgDir.default(process.cwd());
 
     const _path = path.join(rootDir, 'src', 'static', name);
 
+    // check if directory already exists
+    if(fs.existsSync(_path)) {
+        return console.error(messages.generateStatic.exists);
+    }
+
     // create all non-existing directorys
-    await mkdirp(_path);
+    await mkdirp.default(_path);
 
     console.log(messages.generateStatic.success, _path);
 }

d052928bc040a467079a959d3f890a89ef6a0319
kochmarvin <marvin.info@gmx.at>
2021-06-02 13:49:51 +0200

    :shirt: linted

--- grafe-cli/src/start/start.ts
+++ grafe-cli/src/start/start.ts
@@ -6,10 +6,10 @@ import * as shell from 'shelljs';
 import { createDirectoryContents } from '../utils/templating';
 
 export interface StarterTemplateOptions {
-  templatePath: string; // Path to the template
-  templateName: string; // Name of the template
-  projectPath: string; // Path to the project of the user
-  projectName: string; // Name of the project from the user
+    templatePath: string; // Path to the template
+    templateName: string; // Name of the template
+    projectPath: string; // Path to the project of the user
+    projectName: string; // Name of the project from the user
 }
 
 /**
@@ -19,13 +19,13 @@ export interface StarterTemplateOptions {
  * @returns The same Yargs object
  */
 export function startCommand(
-  yargs: yargs.Argv<Record<string, unknown>>
+    yargs: yargs.Argv<Record<string, unknown>>
 ): yargs.Argv<Record<string, unknown>> {
-  return yargs.option('template', {
-    alias: 't',
-    type: 'string',
-    description: 'What template should be used',
-  });
+    return yargs.option('template', {
+        alias: 't',
+        type: 'string',
+        description: 'What template should be used',
+    });
 }
 
 /**
@@ -35,68 +35,68 @@ export function startCommand(
  * @returns Promise<undefined>
  */
 export async function startHandler(
-  argv: Record<string, unknown>
+    argv: Record<string, unknown>
 ): Promise<void> {
-  // get project name from the arguments
-  let projectName = String(argv.projectName);
+    // get project name from the arguments
+    let projectName = String(argv.projectName);
 
-  // if the project name was not provided get it from the user
-  if (projectName.length === 0) {
-    const answers = await inquirer.prompt([
-      {
-        type: 'input',
-        message: 'Whats the name of your project?',
-        name: 'projectName',
-      },
-    ]);
+    // if the project name was not provided get it from the user
+    if (projectName.length === 0) {
+        const answers = await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Whats the name of your project?',
+                name: 'projectName',
+            },
+        ]);
 
-    projectName = answers.projectName;
-  }
+        projectName = answers.projectName;
+    }
 
-  // create the path to the template starter dir
-  const templateStartersPath = path.join(
-    __dirname,
-    '..',
-    '..',
-    'templates',
-    'starters'
-  );
+    // create the path to the template starter dir
+    const templateStartersPath = path.join(
+        __dirname,
+        '..',
+        '..',
+        'templates',
+        'starters'
+    );
 
-  // get the template the user specified
-  const templateType = await getTemplate(templateStartersPath, argv);
+    // get the template the user specified
+    const templateType = await getTemplate(templateStartersPath, argv);
 
-  const projectOptions: StarterTemplateOptions = {
-    projectName: projectName,
-    projectPath: path.join(process.cwd(), projectName),
-    templateName: templateType,
-    templatePath: path.join(templateStartersPath, templateType),
-  };
+    const projectOptions: StarterTemplateOptions = {
+        projectName: projectName,
+        projectPath: path.join(process.cwd(), projectName),
+        templateName: templateType,
+        templatePath: path.join(templateStartersPath, templateType),
+    };
 
-  // create project folder
-  if (!createProjectFolder(projectOptions)) {
-    return;
-  }
+    // create project folder
+    if (!createProjectFolder(projectOptions)) {
+        return;
+    }
 
-  // copy template into projet folder
-  createDirectoryContents(
-    projectOptions.templatePath,
-    projectOptions.projectName,
-    projectOptions
-  );
+    // copy template into projet folder
+    createDirectoryContents(
+        projectOptions.templatePath,
+        projectOptions.projectName,
+        projectOptions
+    );
 
-  // install packages
-  console.log('Installing packages ...');
+    // install packages
+    console.log('Installing packages ...');
 
-  // install the node packages
-  const packagesInstalled = installPackages(projectOptions.projectPath);
+    // install the node packages
+    const packagesInstalled = installPackages(projectOptions.projectPath);
 
-  // check if there was an error when installing the packages
-  if (packagesInstalled.length !== 0) {
-    console.error(packagesInstalled);
-    return;
-  }
+    // check if there was an error when installing the packages
+    if (packagesInstalled.length !== 0) {
+        console.error(packagesInstalled);
+        return;
+    }
 
-  console.log('Project created!');
+    console.log('Project created!');
 }
 
 /**
@@ -106,42 +106,42 @@ export async function startHandler(
  * @returns Promise<string> of the name form the template
  */
 async function getTemplate(
-  templateDirPath: string,
-  argv: Record<string, unknown>
+    templateDirPath: string,
+    argv: Record<string, unknown>
 ): Promise<string> {
-  // get the template from command line agruments
-  let template = String(argv.template);
+    // get the template from command line agruments
+    let template = String(argv.template);
 
-  // read all templates and put them into an array
-  const templateChoises = fs.readdirSync(templateDirPath);
+    // read all templates and put them into an array
+    const templateChoises = fs.readdirSync(templateDirPath);
 
-  // check if the user has specified a template in the arguments
-  if (template === undefined) {
-    // get the template to use from the user
-    template = await getTemplateFromUser(templateChoises);
-  }
+    // check if the user has specified a template in the arguments
+    if (template === undefined) {
+        // get the template to use from the user
+        template = await getTemplateFromUser(templateChoises);
+    }
 
-  // check if the template does not exist
-  if (templateChoises.indexOf(template) === -1) {
-    console.log(`Template "${template}" not found!`);
-    // get the template to use from the user
-    template = await getTemplateFromUser(templateChoises);
-  }
+    // check if the template does not exist
+    if (templateChoises.indexOf(template) === -1) {
+        console.log(`Template "${template}" not found!`);
+        // get the template to use from the user
+        template = await getTemplateFromUser(templateChoises);
+    }
 
-  return template;
+    return template;
 }
 
 async function getTemplateFromUser(templateChoises: string[]): Promise<string> {
-  const answers = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'templateType',
-      message: 'What project template would you like to use?',
-      choices: templateChoises,
-    },
-  ]);
+    const answers = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'templateType',
+            message: 'What project template would you like to use?',
+            choices: templateChoises,
+        },
+    ]);
 
-  return answers.templateType;
+    return answers.templateType;
 }
 
 /**
@@ -153,16 +153,16 @@ async function getTemplateFromUser(templateChoises: string[]): Promise<string> {
  * @returns true if folder was created. False if it already exists
  */
 function createProjectFolder(options: StarterTemplateOptions): boolean {
-  if (fs.existsSync(options.projectPath)) {
-    console.error(
-      `Folder ${options.projectPath} already exists. Delete it or use another name!`
-    );
-    return false;
-  }
+    if (fs.existsSync(options.projectPath)) {
+        console.error(
+            `Folder ${options.projectPath} already exists. Delete it or use another name!`
+        );
+        return false;
+    }
 
-  fs.mkdirSync(options.projectPath);
+    fs.mkdirSync(options.projectPath);
 
-  return true;
+    return true;
 }
 
 /**
@@ -171,18 +171,18 @@ function createProjectFolder(options: StarterTemplateOptions): boolean {
  * @returns string is empty when everything whent fine
  */
 function installPackages(projectFolder: string): string {
-  const hasPackages = fs.existsSync(path.join(projectFolder, 'package.json'));
+    const hasPackages = fs.existsSync(path.join(projectFolder, 'package.json'));
 
-  if (hasPackages) {
-    shell.cd(projectFolder);
-    const result = shell.exec('npm install', {
-      silent: true,
-    });
+    if (hasPackages) {
+        shell.cd(projectFolder);
+        const result = shell.exec('npm install', {
+            silent: true,
+        });
 
-    if (result.code !== 0) {
-      return 'Something whent wrong';
+        if (result.code !== 0) {
+            return 'Something whent wrong';
+        }
+        return '';
     }
-    return '';
-  }
-  return 'No package.json';
+    return 'No package.json';
 }
--- grafe-cli/src/utils/templating.ts
+++ grafe-cli/src/utils/templating.ts
@@ -11,43 +11,43 @@ import { StarterTemplateOptions } from '../start/start';
  * @param options the data that needs to be inserted into the template
  */
 export function createDirectoryContents(
-  templatePath: string,
-  currentFolder: string,
-  options: StarterTemplateOptions
+    templatePath: string,
+    currentFolder: string,
+    options: StarterTemplateOptions
 ): void {
-  const SKIP_FILES = ['node_modules', '.template.json', 'build'];
+    const SKIP_FILES = ['node_modules', '.template.json', 'build'];
 
-  // read all files/folders (1 level) from template folder
-  const filesToCreate = fs.readdirSync(templatePath);
-  // loop each file/folder
-  filesToCreate.forEach((file) => {
-    const origFilePath = path.join(templatePath, file);
+    // read all files/folders (1 level) from template folder
+    const filesToCreate = fs.readdirSync(templatePath);
+    // loop each file/folder
+    filesToCreate.forEach((file) => {
+        const origFilePath = path.join(templatePath, file);
 
-    // get stats about the current file
-    const stats = fs.statSync(origFilePath);
+        // get stats about the current file
+        const stats = fs.statSync(origFilePath);
 
-    // skip files that should not be copied
-    if (SKIP_FILES.indexOf(file) > -1) return;
+        // skip files that should not be copied
+        if (SKIP_FILES.indexOf(file) > -1) return;
 
-    if (stats.isFile()) {
-      // read file content and transform it using template engine
-      let contents = fs.readFileSync(origFilePath, 'utf8');
+        if (stats.isFile()) {
+            // read file content and transform it using template engine
+            let contents = fs.readFileSync(origFilePath, 'utf8');
 
-      // insert dynamic content
-      contents = ejs.render(contents, options);
+            // insert dynamic content
+            contents = ejs.render(contents, options);
 
-      // write file to destination folder
-      const writePath = path.join(process.cwd(), currentFolder, file);
-      fs.writeFileSync(writePath, contents, 'utf8');
-    } else if (stats.isDirectory()) {
-      // create folder in destination folder
-      fs.mkdirSync(path.join(process.cwd(), currentFolder, file));
-      // copy files/folder inside current folder recursively
-      createDirectoryContents(
-        path.join(templatePath, file),
-        path.join(currentFolder, file),
-        options
-      );
-    }
-  });
+            // write file to destination folder
+            const writePath = path.join(process.cwd(), currentFolder, file);
+            fs.writeFileSync(writePath, contents, 'utf8');
+        } else if (stats.isDirectory()) {
+            // create folder in destination folder
+            fs.mkdirSync(path.join(process.cwd(), currentFolder, file));
+            // copy files/folder inside current folder recursively
+            createDirectoryContents(
+                path.join(templatePath, file),
+                path.join(currentFolder, file),
+                options
+            );
+        }
+    });
 }

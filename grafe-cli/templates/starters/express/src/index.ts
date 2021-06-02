0b84cc39b1ec568f30c34e132d3a19a801a2c7ee
streimelstefan <stefan@streimel.com>
2021-06-02 13:35:10 +0200

    :zap: Added grafe core to starter template and added chai and mocha to the dependencies

--- grafe-cli/templates/starters/express/src/index.ts
+++ grafe-cli/templates/starters/express/src/index.ts
@@ -1,14 +1,9 @@
 import express = require('express');
+import { initCore } from '@grafe/grafe-core/build';
 
 const app = express();
 
-// Grafe install midlewares
-
-// Grafe generate routes
-
-// install wrong url catcher
-
-// install errorHandler
+initCore('../grafe.json', app);
 
 // get the port the server should use
 const port = process.env.PORT || 3000;

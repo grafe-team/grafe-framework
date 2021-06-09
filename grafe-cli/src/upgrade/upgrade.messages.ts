fb9460d843822b652c004b2f18e77776e6383b43
kochmarvin <marvin.info@gmx.at>
2021-06-09 16:56:13 +0200

    :sparkles: grafe upgrade command

--- grafe-cli/src/upgrade/upgrade.messages.ts
+++ grafe-cli/src/upgrade/upgrade.messages.ts
@@ -1,8 +1,14 @@
 import 'colors';
 
 export = {
+    commands: {
+        fix: {
+            description: 'Automatically fix occurring issues',
+        },
+    },
     projectType_warn:
         'WARNING'.yellow + ' PROJECT_TYPE has none of these values ["express"]',
+    length_0_warn: 'WARNING'.yellow + ' %s is empty. Please fix manually',
     warn:
         'WARNING'.yellow +
         ' your grafe.json is not 100% right. Please fix manually',
@@ -10,8 +16,9 @@ export = {
         'SUCCESS'.green +
         ' your grafe.json is on the latest stand and has no issues',
     update_grafe: 'UPDATE'.cyan + ' grafe.json fixing ' + '%s'.red + ' issues',
-    type_error:
-        'ERROR'.red +
-        ' %s is not existing or has the wrong type please fix manually',
+    type_error: 'ERROR'.red + ' %s is not existing or has the wrong type',
+    array_error: 'ERROR'.red + ' found wrong object in %s-Array',
     not_grafe: 'The grafe command must be used within a grafe project.',
+    issue_info: 'INFO'.green + ' found ' + '%s'.red + ' issues',
+    confirm: 'Do you want to fix it automatticly',
 };

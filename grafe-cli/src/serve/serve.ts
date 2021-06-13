7e28c47304f54bceb9fd5fe046c0f618735fcdba
streimelstefan <stefan@streimel.com>
2021-06-13 14:54:52 +0200

    :zap: Serve command like discribed in #23 finished test are still missing

--- grafe-cli/src/serve/serve.ts
+++ grafe-cli/src/serve/serve.ts
@@ -1,87 +1,178 @@
-import yargs from "yargs";
+import yargs, { exit } from 'yargs';
 import * as chokidar from 'chokidar';
 import * as pkgDir from 'pkg-dir';
 import * as path from 'path';
-import { cat, exec } from "shelljs";
-import { ChildProcess, execSync, spawn } from "child_process";
+import { cat, exec } from 'shelljs';
+import { ChildProcess, ChildProcessWithoutNullStreams, execSync, spawn } from 'child_process';
 import * as fs from 'fs';
 import 'colors';
+import { executionAsyncResource } from 'async_hooks';
 
 let activeProcess: ChildProcess;
 
-export function serveCommand(yargs: yargs.Argv<Record<string, unknown>>) {
-
-}
+export function serveCommand(yargs: yargs.Argv<Record<string, unknown>>) {}
 
 export async function serveHandler(argv: Record<string, unknown>) {
     const rootDir = await pkgDir.default(process.cwd());
 
-    console.log(`root dir is: ${rootDir}`);
     const watchDir = path.join(rootDir, 'src');
-    console.log(`watch dir is: ${watchDir}`);
 
-    chokidar.watch(watchDir, {ignoreInitial: true}).on('all', (event, path) => {
-        console.log(`event: ${event}`);
-        console.log(`path: ${path}`);
+    chokidar
+        .watch(watchDir, { ignoreInitial: true })
+        .on('all', (event, path) => {
+            console.log('\n-- Change detected restarting --\n'.green);
 
-        compile(rootDir);
-    });
+            compile(rootDir);
+        });
 
     compile(rootDir);
 }
 
-export function compile(rootDir: string) {
+/**
+ * Starts the compiling process. It first deletes everything from the build folder. After that it 
+ * starts the Typescript compiler and compiles. After the compiler finished it starts the backend.
+ * 
+ * This code needs improvemnt and a lot of it...
+ * @param rootDir The root directory of the project
+ */
+export async function compile(rootDir: string) {
     if (activeProcess && !activeProcess.killed) {
-        process.kill(activeProcess.pid);
+        killTask(activeProcess);
     }
 
     try {
         const deleteDirectory = path.join(rootDir, 'build');
-        console.log(`removing files from "${deleteDirectory}" directory.`);
+        console.log('\n-- Cleaning build directory --\n'.green);
         removeEverythingFromDir(deleteDirectory);
     } catch (error) {
-        console.error(`There was an error in the cleansing stage: ${error}`.red);
+        console.error(
+            `\nThere was an error in the cleaning stage: ${error}\n`.red
+        );
+        return;
     }
 
     try {
-        console.log(`compiling useing npm command`);
-        exec('npm run build', {
-            cwd: rootDir,
-        });
+        console.log(`\n-- Compiling code --\n`.green);
+        const code = await compileCode(rootDir);
+
+        if (code !== 0) {
+            console.error(`\n-- There was an error during compiling. Additional information should be above! Compiler finished with code ${code} --\n`.red);
+            return;
+        }
+
     } catch (error) {
-        console.error(`There was an error in the compiling stage: ${error}`.red);
+        console.error(
+            `\nThere was an error in the compiling stage: ${error}\n`.red
+        );
+        return;
     }
 
     try {
-        console.log(`starting backend in async mode ${rootDir}`);
-        activeProcess = exec('npm run start', {
+        console.log(`\n-- Starting project --\n`.green);
+        activeProcess = exec('node build/index.js', {
             cwd: rootDir,
-            async: true
+            async: true,
         });
-
     } catch (error) {
-        console.error(`There was an error in the running stage: ${error}`.red);
+        console.error(`\nThere was an error in the running stage: ${error}\n`.red);
+        return;
     }
-
 }
 
 export function removeEverythingFromDir(dir: string) {
     const files = fs.readdirSync(dir);
 
-    files.forEach(file => {
+    files.forEach((file) => {
         const filePath = path.join(dir, file);
         const stats = fs.statSync(filePath);
 
         if (stats.isFile()) {
             fs.unlinkSync(filePath);
         } else if (stats.isDirectory()) {
-            // node 14.x   
+            // node 14.x
             if (typeof fs.rmSync === 'function') {
-                fs.rmSync(filePath, {recursive: true, force: true});
+                fs.rmSync(filePath, { recursive: true, force: true });
             } else {
                 // node 12.x
-                fs.rmdirSync(filePath, {recursive: true});
+                fs.rmdirSync(filePath, { recursive: true });
             }
         }
+    });
+}
+
+/**
+ * This function kills all the childs of the pid.
+ *
+ * This code is from nodemon: https://github.com/remy/nodemon/blob/master/lib/monitor/run.js
+ *
+ * @param childPid The pid where all childs should be killed
+ * @return void
+ */
+export function killTask(child: ChildProcess): void {
+    // is windows
+    if (process.platform === 'win32') {
+        // When using CoffeeScript under Windows, child's process is not node.exe
+        // Instead coffee.cmd is launched, which launches cmd.exe, which starts
+        // node.exe as a child process child.kill() would only kill cmd.exe, not
+        // node.exe
+        // Therefore we use the Windows taskkill utility to kill the process and all
+        // its children (/T for tree).
+        // Force kill (/F) the whole child tree (/T) by PID (/PID 123)
+        exec('taskkill /pid ' + child.pid + ' /T /F', {silent: true});
+    } else if (process.platform === 'darwin') {
+        // Mac OS
+        child.kill('SIGTERM');
+    } else {
+        // Linux
+        child.kill('SIGTERM');
+    }
+}
+
+async function compileCode(rootDir: string) {
+
+    return new Promise<number>((res, rej) => {
+        let compiler: ChildProcessWithoutNullStreams; 
+        
+        compiler = spawn('tsc', {
+            cwd: rootDir,
+            shell: true
+        });
+
+        compiler.on('error', async error => {
+
+            // @ts-ignore
+            if (error.code === 'ENOENT') {
+
+                console.warn('\n-- Unable to find Typescript globally. Falling back to using `npm run build` --'.yellow);
+
+                try {
+                    res(await compileCodeUsingNodeModules(rootDir));
+                } catch(error) {
+                    rej(error);
+                }
+            }
+        });
+        
+        compiler.stdout.on('data', data => {
+            console.log(`${data}`);
+        });
+        
+        compiler.stderr.on('data', data => {
+            console.log(`${data}`);
+        });
+
+        compiler.on('exit', code => {
+            res(code);
+        })
+    });
+}
+
+async function compileCodeUsingNodeModules(rootDir: string): Promise<number> {
+    return new Promise<number>((res, rej) => {
+        const child = exec('npm run build', {
+            cwd: rootDir,
+        });
+
+        res(child.code);
     });
 }

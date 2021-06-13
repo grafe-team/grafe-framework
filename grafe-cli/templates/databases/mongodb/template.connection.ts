cd54302a7653a259f4e3978aa3ec243dc32b143c
streimelstefan <stefan@streimel.com>
2021-06-13 15:38:53 +0200

    :hammer: Removing template for the databases because they will be added to an add on

--- grafe-cli/templates/databases/mongodb/template.connection.ts
+++
@@ -1,9 +0,0 @@
-import mongoose = require('mongoose');
-
-export function configureDatabase() {
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log('src.index: Successfully connected to the Database!');
-}).catch(err => {
-    console.log(err);
-});
-}

f9f7565740ee3a28259636094bbba1fd3cfa84cb
streimelstefan <stefan@streimel.com>
2021-09-08 19:19:11 +0200

    :construction: Started building the main grafe class over whitch will be the main one the user will interact with

--- grafe-core/src/grafe.ts
+++ grafe-core/src/grafe.ts
@@ -1,12 +1,47 @@
+import { ComponentConfigStore } from "./components/componentConfigStore";
+import { Config } from "./config/config";
+import { Importer } from "./importer/importer";
+import { MiddlewareSpawner } from "./middleware/middlewareSpawner";
+import { RouteSpawner } from "./route/routeImporter";
+import * as path from 'path';
+import getCallerFile = require('get-caller-file');
+import * as fs from 'fs';
+import { LogLevel } from "./logger/logger";
 
-class Grafe {
-    
+export class Grafe {
+    private config: Config;
+
+    public constructor(configPath: string, logLevel: LogLevel = LogLevel.info) {
+        const caller = getCallerFile();
+
+        const configFilePath = path.join(path.dirname(caller), configPath);
+
+        this.config = new Config(logLevel);
+        this.config.projectBase = path.dirname(configFilePath);
+        this.config.codeBase = path.dirname(caller);
+        this.config.parseConfigFile(JSON.parse(fs.readFileSync(configFilePath, 'utf8')));
+
+        this.config.addSpawner('middleware', new MiddlewareSpawner(this.config));
+        this.config.addSpawner('route', new RouteSpawner(this.config));
+    }
 
     public registerAddon(): void {
         throw new Error('This method is not jet implemented');
     }
 
+    public registerRestProvider(): void {
+        throw new Error('This method is not jet implemented');
+    }
+
     public on(hook: string, handler: () => void): void {
         throw new Error('This method is not jet implemented');
+    }
+
+    public build(): void {
+        ComponentConfigStore.getInstance(this.config.logger);
+
+        const importer = new Importer(this.config.logger);
+
+        importer.importModule(path.join(__dirname, 'test'));
     }
 }

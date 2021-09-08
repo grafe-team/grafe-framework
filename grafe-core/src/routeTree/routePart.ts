b7b25f6d1c7057facae63c948162f1da8602e483
streimelstefan <stefan@streimel.com>
2021-09-08 19:18:20 +0200

    :construction: Started working on the RouteTree but there is still a lot that needs to be done here

--- grafe-core/src/routeTree/routePart.ts
+++ grafe-core/src/routeTree/routePart.ts
@@ -1,16 +1,40 @@
+import { RouteHandler } from '../route/routeHandler';
 
 export class RoutePart {
+    private route?: RouteHandler;
 
+    private children: Record<string, RoutePart> = {};
 
+    public constructor(
+        parent: RoutePart,
+        pathPart: string,
+        route?: RouteHandler
+    ) {
+        if (parent !== null || parent !== undefined) {
+            parent.registerChild(this, '/' + pathPart);
+        }
 
-
+        this.route = route;
+    }
 
     public isRoute(): boolean {
-        throw new Error('This method is not implemented yet');
+        return this.route !== undefined && this.route !== null;
     }
 
     public isPart(): boolean {
-        throw new Error('This method is not implemented yet');
+        return !this.isRoute();
+    }
+
+    public getChild(path: string): RoutePart | undefined {
+        return this.children[path];
+    }
+
+    public getAllChildrenPaths(): string[] {
+        return Object.keys(this.children);
+    }
+
+    public registerChild(child: RoutePart, pathPart: string): void {
+        throw new Error('This method is not implemented yet!');
     }
 
 }
--- grafe-core/src/routeTree/routeTree.ts
+++ grafe-core/src/routeTree/routeTree.ts
@@ -1,5 +1,56 @@
-import { RoutePart } from "./routePart";
+import { Importer } from '../importer/importer';
+import { GrafeLogger } from '../logger/logger';
+import { Config } from '../config/config';
+import { RoutePart } from './routePart';
+import * as fs from 'fs';
+import * as path from 'path';
+
+interface FileInfo {
+    path: string;
+    name: string;
+    isDirectory: boolean;
+    isFile: boolean;
+}
 
 export class RouteTree {
-    private root: RoutePart
+
+    private root: RoutePart;
+
+    public build(codeBase: string, importer: Importer, logger: GrafeLogger) {
+        const stats = this.readAllFileStatsOfDir(codeBase);
+    }
+
+    private readAllFileStatsOfDir(dirPath: string) {
+        // check if the path supplied resolves to a directory
+        const dirStats = fs.statSync(dirPath);
+
+        if (!dirStats.isDirectory()) {
+            throw new Error(
+                `Unable to read files of ${dirPath}. It is not a directory!`
+            );
+        }
+
+        const dir = fs.readdirSync(dirPath);
+
+        const result: FileInfo[] = [];
+
+        // read the stats of each file an put them into the FileInfo
+        // Format, for easier administration later on.
+        dir.forEach((fileName) => {
+            const filePath = path.join(dirPath, fileName);
+
+            const fileStat = fs.statSync(filePath);
+
+            const fileInfo = {
+                path: filePath,
+                isDirectory: fileStat.isDirectory(),
+                isFile: fileStat.isFile(),
+                name: fileName,
+            };
+
+            result.push(fileInfo);
+        });
+
+        return result;
+    }
 }
